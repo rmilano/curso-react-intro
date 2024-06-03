ef60a2e5954212c32d10a5a5fdba879463359115
rmilano <rodolfomilano@gmail.com>
2024-06-03 10:56:49 -0400

    update all

---
+++ src/Modal/index.js
@@ -0,0 +1,13 @@
+import React, { Children } from "react";
+import ReactDOM from 'react-dom';
+
+function Modal({ children }){
+    return ReactDOM.createPortal(
+        <div className="Modal">
+            {children}
+        </div>, 
+        document.getElementById('modal')
+    );
+}
+
+export { Modal }
